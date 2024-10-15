e5d7f94f9c7d87e0573dae6e6ddfa637e965a051
Kaung Thu Khant <KaungKhant@go.rmc.edu>
2024-10-14 23:15:34 -0400

    Fixed the session timeout bug. When the user go to link detail page and go back to the mian page, the session timed out and user is kicked out of the account. Updated the /login route to properly set up the session.
    Fixed the style chang bug.
    When visibility chage in the current tab user is in, the application updates the url table but the styles are not appended. Updated index.ejs and styles.css to catch this update and apply the style.

--- server.js
+++ server.js
@@ -275,6 +275,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     }
 });
 
+
 // Route: Confirmation page
 app.get('/confirmation', (req, res) => {
     res.render('confirmation');
